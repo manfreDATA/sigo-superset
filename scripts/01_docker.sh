01971230f3209fec9df030630b3170c651b2b359
Admin <a@b.cl>
2025-09-21 07:27:19 -0300

    fix docker image url

--- scripts/01_docker.sh
+++ scripts/01_docker.sh
@@ -11,7 +11,7 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 . /etc/os-release
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-  https://download.docker.com/linux/ubuntu ${VERSION_CODENAME} stable" \
+  https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
   > /etc/apt/sources.list.d/docker.list
 
 apt-get update -y
